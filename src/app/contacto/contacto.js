632e804b0b13d77293fba19aef4d02277be9efa8
Android Studio <devandroid@ort>
2024-07-03 14:20:47 -0300

    a

---
+++ src/app/contacto/contacto.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Image from "next/image";
+import styles from "./page.module.css";
+
+const Contacto = () => {
+  return (
+    <div>
+      <h1>hola</h1>
+    </div>
+  );
+};
+
+export default Contacto;
---
+++ src/app/reservas/reservas.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Image from "next/image";
+import styles from "./page.module.css";
+
+const Reservas = () => {
+  return (
+    <div>
+      <h1>hola</h1>
+    </div>
+  );
+};
+
+export default Reservas;
